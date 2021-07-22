8f409f97c66fbbb29d2d89fa87899f4be0f62d30
Ranjani <ranjaniaj2002@gmail.com>
2021-07-22 21:53:30 +0530

    Modified BS

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -2,34 +2,47 @@
 
 using namespace std;
 
+#define size 6
+
+int BinarySearch(int arr[],int start,int end,int ele)
+{
+    if(start<=end)
+    {
+        int mid;
+        mid = (start + end)/2;
+        if (arr[mid]==ele)
+        {
+            return mid + 1;
+        }
+        else if(arr[mid]<ele)
+        {
+            return BinarySearch(arr,mid+1,end,ele);
+        }
+        else if(arr[mid]>ele)
+        {
+            return BinarySearch(arr,start,mid-1,ele);
+        }
+    }
+    return -1;
+}
+
 int main()
 {
     cout<<"\t......Binary Search......\n"<<endl;
-    int len,Ele,key;
-    cout <<"Enter the length of the array :";
-    cin>>len;
-    int arr[len];
-    cout <<"Enter "<<len<<" Elements :"<<endl;
-    for(int i=0;i<len;i++)
+    int Ele,key;
+    int arr[size]={12,33,89,98,120,234};
+    cout<<"The array is:"<<endl;
+    for(int i: arr)
     {
-        cout<<"Enter "<<i<<"th element: ";
-        cin>>arr[i];
+        cout<<i<<"  ";
     }
-    cout<<"\nThe array is:"<<endl;
-    for(int i=0;i<len;i++)
-    {
-        cout<<arr[i]<<"  ";
-    }
-
     cout<<"\n\nEnter the Element to be Searched :";
     cin>>Ele;
-    
-    
-    if(key!=0)
-        cout<<Ele<<" is found at Index "<<key<<endl;
+    key = BinarySearch(arr,0,size-1,Ele);    
+    if(key!=-1)
+        cout<<Ele<<" is found at position "<<key<<endl;
     else
-        cout<<Ele <<" is not Index"<<endl;
+        cout<<Ele <<" is not found"<<endl;
     
     return 0;
-
 }
